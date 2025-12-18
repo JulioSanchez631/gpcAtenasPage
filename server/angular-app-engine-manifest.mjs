4a3567219d15fee5a6659adf30786a76b570bc54
Julio Cesar <juli0c3sar300@gmail.com>
2025-12-17 23:05:41 -0600

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://JulioSanchez631.github.io/gpcAtenasPage',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
